2b26b109e35ee7614cf61d2208e785ac663d3434
Kristijan Maksimovski <kikomaksimovski2002@gmail.com>
2025-08-11 19:50:11 +0200

    .....

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,20 +15,20 @@ function MyApp({ Component, pageProps, initialLang }: AppProps & { initialLang: 
     <>
       <Head>
         {/* Favicon */}
-        <link rel="icon" type="image/png" sizes="32x32" href="/front black.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/front black.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/front black.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="https://ik.imagekit.io/egu9w5hi1/front%20black.png?updatedAt=1754934518405" />
+        <link rel="icon" type="image/png" sizes="16x16" href="https://ik.imagekit.io/egu9w5hi1/front%20black.png?updatedAt=1754934518405" />
+        <link rel="apple-touch-icon" sizes="180x180" href="https://ik.imagekit.io/egu9w5hi1/front%20black.png?updatedAt=1754934518405" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#ffffff" />
         
         {/* Open Graph Meta Tags for Social Media Sharing */}
         <meta property="og:title" content="Voltage Elektroinštalacije s.p. - Profesionalne elektroinštalacije" />
         <meta property="og:description" content="Voltage Elektroinštalacije s.p. zagotavlja kakovostne elektro storitve v Sloveniji. Z več kot 10-letnimi izkušnjami smo vaš zanesljiv partner za vse električne potrebe." />
-        <meta property="og:image" content="/front black.png" />
+        <meta property="og:image" content="https://ik.imagekit.io/egu9w5hi1/front%20black.png?updatedAt=1754934518405" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://voltage-elektro.si" />
+        <meta property="og:url" content="https://voltage-ten.vercel.app/" />
         <meta property="og:site_name" content="Voltage Elektroinštalacije s.p." />
         <meta property="og:locale" content="sl_SI" />
         
@@ -36,7 +36,7 @@ function MyApp({ Component, pageProps, initialLang }: AppProps & { initialLang: 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Voltage Elektroinštalacije s.p. - Profesionalne elektroinštalacije" />
         <meta name="twitter:description" content="Voltage Elektroinštalacije s.p. zagotavlja kakovostne elektro storitve v Sloveniji. Z več kot 10-letnimi izkušnjami smo vaš zanesljiv partner za vse električne potrebe." />
-        <meta name="twitter:image" content="/front black.png" />
+        <meta name="twitter:image" content="https://ik.imagekit.io/egu9w5hi1/front%20black.png?updatedAt=1754934518405" />
         
         {/* Additional Meta Tags for Better Sharing */}
         <meta name="description" content="Voltage Elektroinštalacije s.p. zagotavlja kakovostne elektro storitve v Sloveniji. Z več kot 10-letnimi izkušnjami smo vaš zanesljiv partner za vse električne potrebe." />
