2c897afc3ba4e6422fe83cb8fa7d0ce799e0e5db
Kristijan Maksimovski <kikomaksimovski2002@gmail.com>
2025-09-04 17:45:05 +0200

    coment send mail component

--- pages/api/sendMail.ts
+++ pages/api/sendMail.ts
@@ -1,37 +1,37 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import nodemailer from "nodemailer";
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import nodemailer from "nodemailer";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method not allowed" });
-  }
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   if (req.method !== "POST") {
+//     return res.status(405).json({ message: "Method not allowed" });
+//   }
 
-  const { to, subject, text } = req.body;
+//   const { to, subject, text } = req.body;
 
-  try {
-    // конфигурација на SMTP транспортер за Neoserv
-    const transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      port: Number(process.env.MAIL_PORT),
-      secure: true, // за 465 треба true, за 587 false
-      auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
-      },
-    });
+//   try {
+//     // конфигурација на SMTP транспортер за Neoserv
+//     const transporter = nodemailer.createTransport({
+//       host: process.env.MAIL_HOST,
+//       port: Number(process.env.MAIL_PORT),
+//       secure: true, // за 465 треба true, за 587 false
+//       auth: {
+//         user: process.env.MAIL_USER,
+//         pass: process.env.MAIL_PASS,
+//       },
+//     });
 
-    // испраќање на емаил
-    await transporter.sendMail({
-      from: `"My App" <${process.env.MAIL_USER}>`, // од кој е-маил праќаш
-      to, // примач
-      subject, // наслов
-      text, // текст
-      // можеш и html: "<p>Hello <b>World</b></p>"
-    });
+//     // испраќање на емаил
+//     await transporter.sendMail({
+//       from: `"My App" <${process.env.MAIL_USER}>`, // од кој е-маил праќаш
+//       to, // примач
+//       subject, // наслов
+//       text, // текст
+//       // можеш и html: "<p>Hello <b>World</b></p>"
+//     });
 
-    return res.status(200).json({ message: "Mail sent successfully 🚀" });
-  } catch (error) {
-    console.error("Mail error:", error);
-    return res.status(500).json({ message: "Error sending mail" });
-  }
-}
+//     return res.status(200).json({ message: "Mail sent successfully 🚀" });
+//   } catch (error) {
+//     console.error("Mail error:", error);
+//     return res.status(500).json({ message: "Error sending mail" });
+//   }
+// }
